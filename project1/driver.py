eb44aaef96bc41a98d7a644ce6ba0301122e2b3d
David Moratti <dlmoratti@gmail.com>
2021-10-15 17:06:48 -0700

    Added requirements file for requests library, json library. Added functionality to send an argument for apikey and an IP address to ipgeolocate.io. Pretty prints the response

--- project1/driver.py
+++ project1/driver.py
@@ -1,7 +1,20 @@
 import os
 import sys
+import requests
+import json
+print("To use this script, first argument should be the API key you're using for ipgeolocation.io.")
+print("The second argument should be the individual IP you're looking for, in IPv4 notation.")
+print("Since you are passing these via the commandline, an attacker with local access to your machine might be able to look at your bash history to see what IP addresses you've looking at")
 
-print ("This is the command line argument passed ", sys.argv[1])
+
+
 
 print ("Full Argument List: ", str(sys.argv))
+apikey = sys.argv[1]
+ip = sys.argv[2]
 
+r = requests.get('https://api.ipgeolocation.io/ipgeo?apiKey=%s&ip=%s' % (apikey,ip))
+#take the response, load it into json so we can pretty print
+response = json.loads(r.text)
+
+print(json.dumps(response, indent=4, sort_keys=True))
