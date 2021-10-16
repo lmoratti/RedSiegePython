16504ec9095a14f1d9614f32ffa25c83eab5ebdd
dlmoratti <dlmoratti@gmail.com>
2021-10-15 20:30:56 -0700

    Updated README. Added example for project1 with IPs from prompt. Refactored driver.py to use a function for querying API. Added support for reading from files or using a single IP address. Updated instructions on file run to look better in a 80 character terminal.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # RedSiegePython
 The code along for learning infosec python 2021
+
+Each project will have it's own requirements.txt file. This is intended to provide it's dependencies on a per project basis.
--- project1/driver.py
+++ project1/driver.py
@@ -2,19 +2,40 @@ import os
 import sys
 import requests
 import json
-print("To use this script, first argument should be the API key you're using for ipgeolocation.io.")
-print("The second argument should be the individual IP you're looking for, in IPv4 notation.")
-print("Since you are passing these via the commandline, an attacker with local access to your machine might be able to look at your bash history to see what IP addresses you've looking at")
+print("To use this script...")
+print("The first argument should be: \n    The API key you're using for ipgeolocation.io.")
+print("The second argument should be:\n    '0' if you are giving a single IP via commandline\n    '1' if a file with multiple IPs")
+print("The third argument should be:\n    the individual IP you're looking for OR a file name with multiple IPs")
+print("WARNING: You are passing info via the commandline. An attacker with local access to your machine might be able to look at your bash history.")
+print("In which case, they can see what IP addresses you've been looking at or your API key. OpSec is important, folks.")
+
+
+def queryIP(resource):
+	r = requests.get('https://api.ipgeolocation.io/ipgeo?apiKey=%s&ip=%s' % (apikey,resource))
+	#take the response, load it into json so we can prettyprint output
+	response = json.loads(r.text)
+	print(json.dumps(response, indent=4, sort_keys=True))
 
 
 
-
-print ("Full Argument List: ", str(sys.argv))
 apikey = sys.argv[1]
-ip = sys.argv[2]
+isFile = sys.argv[2] #check flag if specifying a file
+resource = sys.argv[3] 
 
-r = requests.get('https://api.ipgeolocation.io/ipgeo?apiKey=%s&ip=%s' % (apikey,ip))
-#take the response, load it into json so we can pretty print
-response = json.loads(r.text)
+#option for single IP passed in
+if isFile == "0":
+	queryIP(resource)
+	print("im here")
 
-print(json.dumps(response, indent=4, sort_keys=True))
+#option for file with multiple IPs passed in
+if isFile == "1":
+	# Using readlines()
+	file1 = open(resource, 'r')
+	lines = file1.readlines()
+	 
+	for line in lines:
+	    queryIP(line)
+
+if isFile != "0" and isFile != "1":
+	print("The second argument should be a '0' if you are giving a single IP, '1' if a file with multiple IPs")
+	#Could optionally throw an exception here
