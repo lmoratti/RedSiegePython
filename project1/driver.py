b7c85522b441a8ee4c22629ee13de2a7508d4bf3
David Moratti <dlmoratti@gmail.com>
2021-10-27 12:35:33 -0700

    ran python black to reformat the code. Thanks Linkavych.

--- project1/driver.py
+++ project1/driver.py
@@ -2,39 +2,53 @@ import os
 import sys
 import requests
 import json
+
 print("To use this script...")
-print("The first argument should be: \n    The API key you're using for ipgeolocation.io.")
-print("The second argument should be:\n    '0' if you are giving a single IP via commandline\n    '1' if a file with multiple IPs")
-print("The third argument should be:\n    the individual IP you're looking for OR a file name with multiple IPs")
-print("WARNING: You are passing info via the commandline. An attacker with local access to your machine might be able to look at your bash history.")
-print("In which case, they can see what IP addresses you've been looking at or your API key. OpSec is important, folks.")
+print(
+    "The first argument should be: \n    The API key you're using for ipgeolocation.io."
+)
+print(
+    "The second argument should be:\n    '0' if you are giving a single IP via commandline\n    '1' if a file with multiple IPs"
+)
+print(
+    "The third argument should be:\n    the individual IP you're looking for OR a file name with multiple IPs"
+)
+print(
+    "WARNING: You are passing info via the commandline. An attacker with local access to your machine might be able to look at your bash history."
+)
+print(
+    "In which case, they can see what IP addresses you've been looking at or your API key. OpSec is important, folks."
+)
 
 
 def queryIP(resource):
-	r = requests.get('https://api.ipgeolocation.io/ipgeo?apiKey=%s&ip=%s' % (apikey,resource))
-	#take the response, load it into json so we can prettyprint output
-	response = json.loads(r.text)
-	print(json.dumps(response, indent=4, sort_keys=True))
-
+    r = requests.get(
+        "https://api.ipgeolocation.io/ipgeo?apiKey=%s&ip=%s" % (apikey, resource)
+    )
+    # take the response, load it into json so we can prettyprint output
+    response = json.loads(r.text)
+    print(json.dumps(response, indent=4, sort_keys=True))
 
 
 apikey = sys.argv[1]
-isFile = sys.argv[2] #check flag if specifying a file
-resource = sys.argv[3] 
+isFile = sys.argv[2]  # check flag if specifying a file
+resource = sys.argv[3]
 
-#option for single IP passed in
+# option for single IP passed in
 if isFile == "0":
-	queryIP(resource)
+    queryIP(resource)
 
-#option for file with multiple IPs passed in
+# option for file with multiple IPs passed in
 if isFile == "1":
-	# Using readlines()
-	file1 = open(resource, 'r')
-	lines = file1.readlines()
-	 
-	for line in lines:
-	    queryIP(line)
+    # Using readlines()
+    file1 = open(resource, "r")
+    lines = file1.readlines()
+
+    for line in lines:
+        queryIP(line)
 
 if isFile != "0" and isFile != "1":
-	print("The second argument should be a '0' if you are giving a single IP, '1' if a file with multiple IPs")
-	#Could optionally throw an exception here
+    print(
+        "The second argument should be a '0' if you are giving a single IP, '1' if a file with multiple IPs"
+    )
+    # Could optionally throw an exception here
--- project2/driver.py
+++ project2/driver.py
@@ -3,7 +3,8 @@
 import sys
 import requests
 
-def checkheader(webresponse,header):
+
+def checkheader(webresponse, header):
     """This function will check a response from a webserver
     for a specific header supplied as a string"""
     value = webresponse.headers.get(header)
@@ -12,8 +13,10 @@ def checkheader(webresponse,header):
         return True
     print(f"    [FAIL]       NO '{header}' Header Present")
     return False
+
+
 def checkhost(hostresponse):
-    """ This function will check a host for the security
+    """This function will check a host for the security
     headers it deems necessary. It takes a webservers response as an argument"""
     ipaddress = hostresponse.raw._connection.sock.getpeername()[0]
     hostname = str(hostresponse.url)
@@ -21,29 +24,34 @@ def checkhost(hostresponse):
     print(f"    IP: {ipaddress}")
     print("")
     if "https" in hostname:
-        checkheader(hostresponse,'Strict-Transport-Security')
+        checkheader(hostresponse, "Strict-Transport-Security")
     else:
-        print("    [FAIL]       Connected over HTTP,ignoring 'Strict-Transport-Security' check")
-    checkheader(hostresponse,'Content-Security-Policy')
-    checkheader(hostresponse,'X-Frame-Options')
-    checkheader(hostresponse,'Server')
+        print(
+            "    [FAIL]       Connected over HTTP,ignoring 'Strict-Transport-Security' check"
+        )
+    checkheader(hostresponse, "Content-Security-Policy")
+    checkheader(hostresponse, "X-Frame-Options")
+    checkheader(hostresponse, "Server")
 
-#take commandline arguments
-print("Usage: driver.py [Resource: URL of host OR filename] [isFile: expected 0 or 1]\n\n\n")
+
+# take commandline arguments
+print(
+    "Usage: driver.py [Resource: URL of host OR filename] [isFile: expected 0 or 1]\n\n\n"
+)
 resource = sys.argv[1]
-isFile = sys.argv[2] # 0 if cmd provided host, 1 if file with hosts
+isFile = sys.argv[2]  # 0 if cmd provided host, 1 if file with hosts
 if isFile == "0":
-    #we add stream = True to be able to grab the IP for Intermediate Task
-    response = requests.get(resource,stream=True)
+    # we add stream = True to be able to grab the IP for Intermediate Task
+    response = requests.get(resource, stream=True)
     checkhost(response)
-if isFile =="1":
+if isFile == "1":
     # Using readlines()
-    with open(resource, 'r', encoding ='utf-8') as file1:
+    with open(resource, "r", encoding="utf-8") as file1:
         lines = file1.readlines()
         print(f"Checking all hosts in {resource} for security headers")
         for line in lines:
-            line= line.rstrip() #kept getting a '%0a' newline messing with my files
-            #we add stream = True to be able to grab the IP for Intermediate Task
-            response = requests.get(line,stream=True)
+            line = line.rstrip()  # kept getting a '%0a' newline messing with my files
+            # we add stream = True to be able to grab the IP for Intermediate Task
+            response = requests.get(line, stream=True)
             checkhost(response)
             print("\n")
